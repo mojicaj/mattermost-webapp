4cf18d0287af0ee75f84436b709e7323a9b5c244
Jesse Hallam <jesse.hallam@gmail.com>
2018-09-24 17:34:15 -0400

    don't re-emit the notification once shown

--- components/login/login_controller/login_controller.jsx
+++ components/login/login_controller/login_controller.jsx
@@ -137,7 +137,7 @@ class LoginController extends React.Component {
         const search = new URLSearchParams(this.props.location.search);
         const extra = search.get('extra');
 
-        if (extra === Constants.SESSION_EXPIRED) {
+        if (extra === Constants.SESSION_EXPIRED && !this.closeSessionExpiredNotification) {
             Utils.showNotification({
                 title: this.props.siteName,
                 body: Utils.localizeMessage(
@@ -154,7 +154,7 @@ class LoginController extends React.Component {
             }).catch(() => {
                 // Ignore the failure to display the notification.
             });
-        } else if (this.closeSessionExpiredNotification) {
+        } else if (extra !== Constants.SESSION_EXPIRED && this.closeSessionExpiredNotification) {
             this.closeSessionExpiredNotification();
             this.closeSessionExpiredNotification = null;
         }
